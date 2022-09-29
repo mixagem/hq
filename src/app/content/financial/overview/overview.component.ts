94523d37fbfe3671b087d59aee301ade67afac8e
mixagem <filipeteixeira141@gmail.com>
2022-09-30 00:47:36 +0100

    leg

--- src/app/content/financial/overview/overview.component.ts
+++ src/app/content/financial/overview/overview.component.ts
@@ -26,10 +26,7 @@ export class OverviewComponent implements OnInit {
 
   constructor(public financialService: FinancialService) {
     const currentDate = new Date();
-    // primeiro, obtemos o mes atual, e o numero de dias que o mes tem
-    // depois fazemos query ao movimentos tesouraria para o mes que estamos
-
-    switch (currentDate.getMonth()+1) {
+    switch (currentDate.getMonth() + 1) {
       case 1: case 3: case 5: case 7: case 8: case 10: case 12:
         this.monthDays = 31;
         break;
@@ -37,11 +34,13 @@ export class OverviewComponent implements OnInit {
         this.monthDays = 30;
         break;
       case 2:
-        this.monthDays = 28;
+        (currentDate.getFullYear() / 4 % 0) ? this.monthDays = 29 : this.monthDays = 28;
         break;
     }
     this.tempArray = Array(this.monthDays).fill(0)
     this.movTes = [
+      { date: new Date('August 5 , 1975'), value: 370, cat: 'second', subcat: 'daas', obs: 'sd' },
+      { date: new Date('August 15 , 1975'), value: 50, cat: 'third', subcat: 'katutz', obs: 'sd' },
       { date: new Date(), value: 110, cat: 'first', subcat: 'az', obs: 's' },
       { date: new Date('August 1, 1975'), value: 220, cat: 'first', subcat: 'ew', obs: 's' }]
   }
@@ -49,29 +48,31 @@ export class OverviewComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  getEmptymambo(){
+  emptyCell() {
     return 0
   }
 
-  getmambo(cellDay:number, cat:string): number{
-    const teste = this.movTes.filter(mov => mov.date.getDate() === cellDay);
+  getDailySum(day: number) {
+    const dailyMovs = this.movTes.filter(mov => mov.date.getDate() === day);
     let value = 0;
-    if (teste.length > 0){
-      teste.forEach(mov => {
-        if(mov.cat === cat)
-        value += mov.value
-      });
-    }
+    if (dailyMovs.length > 0) { dailyMovs.forEach(mov => { value += mov.value }); }
     return value
   }
 
-  getSubmambo(cellDay:number, subcat:string): number{
-    const teste = this.movTes.filter(mov => mov.date.getDate() === cellDay);
+  getCatValue(day: number, cat: string): number {
+    const dailyCatMovs = this.movTes.filter(mov => mov.date.getDate() === day);
     let value = 0;
-    if (teste.length > 0){
-      teste.forEach(mov => {
-        if(mov.subcat === subcat)
-        value += mov.value
+    if (dailyCatMovs.length > 0) { dailyCatMovs.forEach(mov => { if (mov.cat === cat) { value += mov.value } }); }
+    return value
+  }
+
+  getSubcatValue(day: number, cat: string, subcat: string): number {
+    const dailySubCatMovs = this.movTes.filter(mov => mov.date.getDate() === day);
+    let value = 0;
+    if (dailySubCatMovs.length > 0) {
+      dailySubCatMovs.forEach(mov => {
+        if (mov.subcat === subcat && mov.cat === cat)
+          value += mov.value
       });
     }
     return value
