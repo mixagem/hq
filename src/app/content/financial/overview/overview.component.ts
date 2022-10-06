d6fa25353cc67e5c4fb96b31bc2f102544091192
mixagem <filipeteixeira141@gmail.com>
2022-10-06 01:25:00 +0100

    february fix

--- src/app/content/financial/overview/overview.component.ts
+++ src/app/content/financial/overview/overview.component.ts
@@ -38,7 +38,9 @@ export class OverviewComponent implements OnInit {
 
   ngOnInit(): void {
     this.selectedMonth = this.currentDate.getMonth() + 1
+    console.log(this.selectedMonth)
     switch (this.selectedMonth) {
+
       case 1: case 3: case 5: case 7: case 8: case 10: case 12:
         this.monthDays = 31;
         break;
@@ -46,7 +48,7 @@ export class OverviewComponent implements OnInit {
         this.monthDays = 30;
         break;
       case 2:
-        (this.currentDate.getFullYear() / 4 % 0) ? this.monthDays = 29 : this.monthDays = 28;
+        (this.currentDate.getFullYear() % 4 === 0) ? this.monthDays = 29 : this.monthDays = 28;
         break;
     }
     this.selectedYear = this.currentDate.getFullYear();
@@ -59,7 +61,7 @@ export class OverviewComponent implements OnInit {
     // debugger;
     let currentMonth = Number(this.currentDate.toISOString().slice(5, 7))
     if (currentMonth === 12) {
-      let nextYear = Number(this.currentDate.toISOString().slice(0, 5)) + 1
+      let nextYear = Number(this.currentDate.toISOString().slice(0, 4)) + 1
       this.currentDate = new Date(this.currentDate.toISOString().replace(/[0-9]{4}-[0-9]{2}/, nextYear + '-01'))
     } else {
       let nextMonth: string;
@@ -73,11 +75,11 @@ export class OverviewComponent implements OnInit {
     // debugger;
     let currentMonth = Number(this.currentDate.toISOString().slice(5, 7))
     if (currentMonth === 1) {
-      let previousYear = Number(this.currentDate.toISOString().slice(0, 5)) - 1
+      let previousYear = Number(this.currentDate.toISOString().slice(0, 4)) - 1
       this.currentDate = new Date(this.currentDate.toISOString().replace(/[0-9]{4}-[0-9]{2}/, previousYear + '-12'))
     } else {
       let previousMonth: string;
-      currentMonth > 10 ? previousMonth = (currentMonth - 1).toString() : previousMonth = '0' + (currentMonth - 1) ;
+      currentMonth > 10 ? previousMonth = (currentMonth - 1).toString() : previousMonth = '0' + (currentMonth - 1);
       this.currentDate = new Date(this.currentDate.toISOString().replace(/-[0-9]{2}-/, '-' + previousMonth + '-'))
     }
     this.ngOnInit();
