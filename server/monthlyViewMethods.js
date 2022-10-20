b565a519485d22f77f74f0babbf6a3fb42eab8b7
mixagem <filipeteixeira141@gmail.com>
2022-10-20 15:49:20 +0100

    bugfix monthlyview

--- server/monthlyViewMethods.js
+++ server/monthlyViewMethods.js
@@ -54,9 +54,6 @@ export function dailySubCatDetails(req, res) {
 
   const dataIniMilli = dataIni.getTime();
   const dataFiMilli = dataFi.getTime() - 1;
-
-  console.log(new Date(dataIniMilli).toLocaleString('pt'))
-  console.log(new Date(dataFiMilli).toLocaleString('pt'))
   let dailyMovments = [];
 
   db.serialize(() => {
@@ -93,10 +90,6 @@ export function dailyCatDetails(req, res) {
   const dataIniMilli = dataIni.getTime();
   const dataFiMilli = dataFi.getTime() - 1;
 
-
-  console.log(new Date(dataIniMilli).toLocaleString('pt'))
-  console.log(new Date(dataFiMilli).toLocaleString('pt'))
-
   let dailyMovments = [];
 
   db.serialize(() => {
@@ -115,79 +108,71 @@ export function dailyCatDetails(req, res) {
 //snapshots das categorias / subcategorias
 export function monthlySnapshots(req, res) {
 
-  const month = Number(req.body.month) - 1
-  const year = Number(req.body.year)
-
-  let db = new sqlite3.Database('./mhq.db', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) { console.error(err.message); }
-    console.log('Generating categories snapshots...');
-  });
-
-  const dataIni = new Date('2022-01-01T00:00:00.000Z');
-  dataIni.setFullYear(year, month)
-
-  const dataFi = new Date('2022-01-01T00:00:00.000Z');
-  dataFi.setFullYear(year, month + 1)
-
-  const dataIniMilli = dataIni.getTime();
-  const dataFiMilli = dataFi.getTime() - 1;
-
+  const MONTH_DAYS = Number(req.body.monthdays);
+  const MONTH = Number(req.body.month) - 1;
+  const YEAR = Number(req.body.year);
+  const DATA_INI = new Date('2022-01-01T00:00:00.000Z');
+  const DATA_FINI = new Date('2022-01-01T00:00:00.000Z');
+  DATA_INI.setFullYear(YEAR, MONTH); const DATA_INI_MS = DATA_INI.getTime();
+  DATA_FINI.setFullYear(YEAR, MONTH + 1); const DATA_FINI_MS = DATA_FINI.getTime() - 1;
 
   let categoryList = [];
   let subCategoryList = [];
 
+  let db = new sqlite3.Database('./mhq.db', sqlite3.OPEN_READWRITE, (err) => { if (err) { console.error(err.message); } });
   db.serialize(() => {
     db.each(`SELECT * FROM categories WHERE active = 'true' `, (err, row) => { err ? console.error(err.message) : categoryList.push(row.id) })
       .each(`SELECT * FROM subcategories WHERE active = 'true' `, (err, row) => { err ? console.error(err.message) : categoryList.includes(row.maincatid) ? subCategoryList.push(row.id) : [] })
-
   })
+  db.close((err) => { err ? console.error(err.message) : fetchMovments(); });
 
-  db.close((err) => {
-    err ? console.error(err.message) : generateSnapshots();
-  });
-
-
-  let monthlyMovments;
+  let monthlyMovments = [];
   let generatedCategorySnapshots = {};
   let generatedSubCategorySnapshots = {};
 
-  function generateSnapshots() {
-    let query = `SELECT * FROM treasurylog WHERE date <= ${dataFiMilli} AND date >= ${dataIniMilli}`
+  function fetchMovments() {
+    const QUERY = `SELECT * FROM treasurylog WHERE date <= ${DATA_FINI_MS} AND date >= ${DATA_INI_MS}`
     let queryExtra = '';
 
-
     categoryList.forEach((category, i) => {
-      i === 0 ? queryExtra += ` AND cat='${category}'` : queryExtra += ` OR cat='${category}'`;
-      generatedCategorySnapshots[`${category}`] = new Array(req.body.monthdays).fill(0)
+      i === 0 ? queryExtra += ` AND ( cat='${category}'` : queryExtra += ` OR cat='${category}'`;
+      if (i === categoryList.length - 1) { queryExtra += ' )' }
+      generatedCategorySnapshots[`${category}`] = Array(MONTH_DAYS).fill(0)
     });
 
     subCategoryList.forEach((subcategory, i) => {
-      i === 0 ? queryExtra += ` AND subcat='${subcategory}'` : queryExtra += ` OR subcat='${subcategory}'`;
-      generatedSubCategorySnapshots[`${subcategory}`] = new Array(req.body.monthdays).fill(0)
+      i === 0 ? queryExtra += ` AND ( subcat='${subcategory}'` : queryExtra += ` OR subcat='${subcategory}'`;
+      if (i === subCategoryList.length - 1) { queryExtra += ' )' }
+      generatedSubCategorySnapshots[`${subcategory}`] = Array(MONTH_DAYS).fill(0)
     });
 
+    let db = new sqlite3.Database('./mhq.db', sqlite3.OPEN_READWRITE, (err) => { if (err) { console.error(err.message); } });
     db.serialize(() => {
-      db.each(`${query}${queryExtra}`, (err, row) => { err ? console.error(err.message) : monthlyMovments.push(row) })
+      db.each(`${QUERY}${queryExtra}`, (err, row) => { err ? console.error(err.message) : monthlyMovments.push(row) })
     });
 
-    monthlyMovments.forEach(movement => {
-      if (movement.type === 'expense') {
-        generatedCategorySnapshots[`${movement.cat}`][new Date(movement.date).getDate() - 1] -= movement.value
-        generatedSubCategorySnapshots[`${movement.subcat}`][new Date(movement.date).getDate() - 1] -= movement.value
-      } else {
-        generatedCategorySnapshots[`${movement.cat}`][new Date(movement.date).getDate() - 1] += movement.value
-        generatedSubCategorySnapshots[`${movement.subcat}`][new Date(movement.date).getDate() - 1] += movement.value
-      }
-
-    })
-
-    let dailySumEvo = Array(Number(req.body.monthdays)).fill(0)
-    monthlyMovments.forEach(movement => {
-      movement.type === 'expense' ? dailySumEvo[(new Date(movement.date).getDate()) - 1] -= movement.value : dailySumEvo[(new Date(movement.date).getDate()) - 1] += movement.value
+    db.close((err) => {
+      err ? console.error(err.message) : generateSnapshots();
     });
 
-    console.log('Dailysum snapshot sucessfully generated.');
-    res.send([generatedCategorySnapshots, generatedSubCategorySnapshots, dailySumEvo])
+    function generateSnapshots() {
+      monthlyMovments.forEach(movement => {
+        if (movement.type === 'expense') {
+          generatedCategorySnapshots[`${movement.cat}`][Number(new Date(movement.date).getDate()) - 1] -= movement.value
+          generatedSubCategorySnapshots[`${movement.subcat}`][Number(new Date(movement.date).getDate()) - 1] -= movement.value
+        } else {
+          generatedCategorySnapshots[`${movement.cat}`][Number(new Date(movement.date).getDate()) - 1] += movement.value
+          generatedSubCategorySnapshots[`${movement.subcat}`][Number(new Date(movement.date).getDate()) - 1] += movement.value
+        }
+      })
+
+      let dailySumEvo = Array(Number(req.body.monthdays)).fill(0)
+      monthlyMovments.forEach(movement => {
+        movement.type === 'expense' ? dailySumEvo[(new Date(movement.date).getDate()) - 1] -= movement.value : dailySumEvo[(new Date(movement.date).getDate()) - 1] += movement.value
+      });
+
+      res.send([generatedCategorySnapshots, generatedSubCategorySnapshots, dailySumEvo])
+    }
 
 
 
